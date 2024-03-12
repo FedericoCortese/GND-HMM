2ef6d4794501938b5cc557e606f466e1ce4bdef9
Federico P. Cortese <cortese.federico@live.it>
2024-03-12 15:57:31 +0100

    input par of dim 4 (prev 6)

--- GND-HMM/GND functions.R
+++ GND-HMM/GND functions.R
@@ -1,17 +1,24 @@
+
+# GND functions ---------------------------------------------------------------------
+
 dGND=function(theta,eta,y1,y2,logd=T){
   
   # dGND computes density for a bivariate generalized normal distributed vector
-  # theta is the vector of parameters
+  # theta is the vector of parameters, first element is the correlation coefficient, the second, third, and fourth are the parameters corresponding to theta4, theta5, and theta6 of the GND
   # eta is the normalizing constant
   # y1 and y2 are vectors of time-series
   # logd=T returns log of the density
   
-  theta1=theta[1]
-  theta2=theta[2]
-  theta3=theta[3]
-  theta4=theta[4]
-  theta5=theta[5]
-  theta6=theta[6]
+  rho=theta[1]
+  theta1=1/(1-rho^2)
+  theta2=theta1
+  theta3=rho/(1-rho^2)
+  # theta1=theta[1]
+  # theta2=theta[2]
+  # theta3=theta[3]
+  theta4=theta[2]
+  theta5=theta[3]
+  theta6=theta[4]
   
   f=exp((-1/2)*theta1*y1^2-(1/2)*theta2*y2^2+theta3
         *y1*y2+theta4*y1*y2^2+theta5
@@ -27,6 +34,7 @@ dGND=function(theta,eta,y1,y2,logd=T){
 compute_theta=function(rho,theta4=0,theta5=0,theta6=0){
   
   # compute_theta computes theta1, theta2, and theta3 starting from the linear correlation coefficient rho
+  # It returns a vector of parameters of dimension 6
   
   theta=rep(0,6)
   theta[1]=1/(1-rho^2)
@@ -39,19 +47,19 @@ compute_theta=function(rho,theta4=0,theta5=0,theta6=0){
   return(theta)
 }
 
-cond_mom=function(yA,yB,theta){
+cond_mom=function(y1,y2,theta){
   
   # cond_mom computes conditional first and second order moments
-  # yA is the vector of observations
-  # yB is the vector of observations for the conditioning variable
+  # y1 is the vector of observations
+  # y2 is the vector of observations for the conditioning variable
   # theta is the vector of the GND parameters
   
   check_cond1=theta[5]^2-2*theta[6]*theta[1]<0
   check_cond2=theta[4]^2-2*theta[6]*theta[2]<0
   
   if(check_cond1&check_cond2){
-    sig2AB=1/(theta[1]+2*theta[6]*yB^2)
-    muAB=(theta[3]*yB+theta[4]*yB^2)*sig2AB
+    sig2AB=1/(theta[1]+2*theta[6]*y2^2-2*theta[5]*y)
+    muAB=(theta[3]*y2+theta[4]*y2^2)*sig2AB
     return(list(sig2AB=sig2AB,
                 muAB=muAB))
   }
@@ -60,28 +68,139 @@ cond_mom=function(yA,yB,theta){
   }
 }
 
-gfun_GND=function(y,theta,sig2AB){
+gfun_GND=function(y,theta
+                  #,sig2AB
+){
   # This function returns f(y)*exp(eta)
-  g=(2*pi)/sqrt(theta[2])*exp((1/2)*(theta[3]*y+theta[4]*y^2)^2*sig2AB)*sqrt(sig2AB*dnorm(y,sd=1/theta[2]))
+  # y is the vector of observations 
+  # theta is the vector of parameters with 6 components, the first is the correlation coefficient, the second, third, and fourth are the parameters corresponding to theta4, theta5, and theta6 of the GND
+  
+  rho=theta[1]
+  theta1=1/(1-rho^2)
+  theta2=theta1
+  theta3=rho/(1-rho^2)
+  theta4=theta[2]
+  theta5=theta[3]
+  theta6=theta[4]
+  
+  check_cond1=theta5^2-2*theta6*theta1<0
+  check_cond2=theta4^2-2*theta6*theta2<0
+  
+  
+  if(check_cond1&check_cond2){
+    sig2AB=1/(theta1+2*theta6*y^2-2*theta5*y)
+    g=(2*pi)/sqrt(theta2)*exp((1/2)*(theta3*y+theta4*y^2)^2*sig2AB)*
+      sqrt(sig2AB*dnorm(y,sd=1/theta2))
+  }
+  else{
+    stop("Inequality conditions not satisfied")
+  }
   return(g)
 }
 
-get_eta=function(y1,y2,theta){
+get_eta=function(
+    #y1,y2,
+  theta){
   
   # get_eta computes the normalizing constant of the GND density
+  # y1 and y2 are vectors of observations
+  # theta is the vector of parameters with 6 components
   
-  sig2AB=cond_mom(y1,y2,theta)
-  sig2AB=sig2AB$sig2AB
+  # sig2AB=cond_mom(y1,y2,theta)
+  # sig2AB=sig2AB$sig2AB
   
   # Set integral boundaries to -5 and 5 for convergence 
-  int=integrate(f=gfun_GND,lower = -5,upper = 5,theta=theta,sig2AB=sig2AB)
+  int=integrate(f=gfun_GND,lower = -5,upper = 5,theta=theta
+                #,sig2AB=sig2AB
+                )
   eta=log(int$value)
   return(eta)
 }
 
+
+# Simulations ---------------------------------------------------------------------
+
+GND_sim<-function(theta,T){
+  
+  #MC Simulations for the GND model
+  #theta is the vector of parameters with the following components:
+  #theta[1] is the correlation coefficient
+  #theta[2:4] are theta_4, theta_5, and theta_6
+  #T is the number of observations
+  
+  # theta1=theta[1]
+  # theta2=theta[2]
+  # theta3=theta[3]
+  # 
+  theta4=theta[2]
+  theta5=theta[3]
+  theta6=theta[4]
+  rho=theta[1]
+  theta1<-theta2<-1/(1-rho^2)
+  theta3<-rho*theta1
+  
+  stheta1<-stheta2<-1/sqrt(theta1)
+  
+  #Griddy-Gibbs for Y2:
+  ngrid=10000 
+  grid=seq(from=-8,to=8,length.out=ngrid)
+  coef1=theta1+2*theta6*grid^2-2*theta5*grid
+  sigma2_12=1/coef1
+  ee1=exp(.5*(theta3*grid+theta4*grid^2)^2*sigma2_12 )
+  pp=dnorm(grid,mean=0,sd=stheta1)*ee1*sqrt(sigma2_12)
+  pp=pp/sum(pp)
+  i = sample(ngrid, T, replace = TRUE, prob = pp)
+  dx = diff(grid[1:2])
+  Y2 = grid[i] + runif(T) * dx - dx/2
+  coef1=theta1+2*theta6*Y2^2-2*theta5*Y2
+  sigma2_12=1/coef1
+  mu_12=sigma2_12*(theta3*Y2+theta4*Y2^2)
+  Y1=rnorm(T,mean=mu_12,sd=sqrt(sigma2_12))
+  return(cbind(Y1,Y2))
+}
+
+
+
+GNDHMM_sim=function(n,THETA, Q, init,seed){
+  # This function simulates a GND-HMM model
+  # n is the number of observations
+  # THETA is a matrix of dimension 4 x k where k is the number of regimes, so that each column is the state specific vector of parameters 
+  # Q is the transition matrix
+  # init is the initial distribution
+  # seed is the seed for the random number generator
+  
+  #markov chain simulation
+  reg = dim(Q)[1]
+  x <- numeric(n)
+  set.seed(seed)
+  x[1] <- sample(1:reg, 1, prob = init)
+  for(i in 2:n){
+    x[i] <- sample(1:reg, 1, prob = Q[x[i - 1], ])
+  }
+  
+  d=2
+  Sim = matrix(0, n, d * reg)
+  SimData = matrix(0, n, d)
+  
+  #pseudo-observations simulation
+  for (k in 1:reg) {
+    u=GND_sim(THETA[,k],n)
+    #u = rCopula(n, copula::tCopula(param=P2p(R[,,k]), dim = d,df=nu[k],dispstr = "un"))
+    Sim[, (d * k - d + 1):(d * k)] = u
+  }
+  
+  for (i in 1:n) {
+    k = x[i]
+    SimData[i, ] = Sim[i, (d * k - d + 1):(d * k)]
+  }
+  return(list(SimData=SimData,states=x))
+  
+}
+
+
 # EM ---------------------------------------------------------------------
 
-EstGNDHMM=function (y1,y2, theta, Q){
+EstGNDHMM=function (y1,y2, THETA, Q){
   n = dim(y)[1]
   r = dim(Q)[2]
   eta_bar = matrix(0, n, r)
@@ -92,9 +211,9 @@ EstGNDHMM=function (y1,y2, theta, Q){
   M = matrix(0, r, r)
   
   for(j in 1:reg){
-    c[, j] = dGND(theta[j,],et[j],y1,y2,logd=T)
+    c[, j] = dGND(THETA[j,],et[j],y1,y2,logd=T)
   }
-
+  
   ###
   eta_bar[n, ] = 1/r
   for (k in 1:(n - 1)) {
@@ -136,14 +255,14 @@ EstGNDHMM=function (y1,y2, theta, Q){
   
   fun = function(thetaa) {
     if (r < 2) {
-      log_likelihood = -sum(lambda[, 1] * dGND(theta[1,],eta[1],y1,y2,logd=T)
+      log_likelihood = -sum(lambda[, 1] * dGND(theta[1,],et[1],y1,y2,logd=T)
       )
     } else if (r > 1) {
-      log_likelihood = -sum(lambda[, 1] * dGND(theta[1,],eta[1],y1,y2,logd=T))
+      log_likelihood = -sum(lambda[, 1] * dGND(theta[1,],et[1],y1,y2,logd=T))
       
       for (i in 2:r) {
         log_likelihood = log_likelihood + (-sum(lambda[, 
-                                                       i] * dGND(theta[i,],eta[i],y1,y2,logd=T)
+                                                       i] * dGND(theta[i,],et[i],y1,y2,logd=T)
         )
         )
       }
@@ -160,5 +279,4 @@ EstGNDHMM=function (y1,y2, theta, Q){
              eta_bar = eta_bar, lambda = lambda, Lambda = Lambda)
   return(out)
 }
-
 
--- GND-HMM/run.R
+++ GND-HMM/run.R
@@ -1,9 +1,83 @@
+## univariate trials
+
+source("GND functions.R")
+
+rho=.5
+thetaA=c(rho,1,1,1)
+prv=GND_sim(thetaA,T=1000)
+thetaB=c(rho,0,0,2)
+prv2=GND_sim(thetaB,T=1000)
+par(mfrow=c(1,2))
+plot(prv[,1],prv[,2],xlab="Y1",ylab="Y2",main="Simulated GND")
+plot(prv2[,1],prv2[,2],xlab="Y1",ylab="Y2",main="Simulated GND")
+
+THETA=cbind(thetaA,thetaB)
+n=1000
+Q=matrix(c(.9,.1,.1,.9),2,2)
+init=c(.5,.5)
+seed=123
+
+sim=GNDHMM_sim(1000,THETA,Q,init,seed)
+par(mfrow=c(1,1))
+plot(sim$SimData[,1],sim$SimData[,2],col=sim$states,xlab="Y1",ylab="Y2",
+     main="Simulated GND-HMM",pch=19)
+legend("topright",legend=c("Regime 1","Regime 2"),col=1:2,pch=19)
+
+y1=sim$SimData[,1]
+y2=sim$SimData[,2]
+
+
+fun = function(thetaa) {
+  #THETA=matrix(thetaa,ncol=r)
+  
+  #theta=compute_theta(theta[1],theta[2],theta[3],theta[4])  
+  eta=get_eta(thetaa)
+  log_likelihood = -sum(dGND(thetaa,eta,y1,y2,logd=T))
+  # for (i in 2:r) {
+  #   eta=get_eta(THETA[,i])
+  #   log_likelihood = log_likelihood + (-sum(dGND(THETA[,i],eta,y1,y2,logd=T)))
+  # }
+  
+  return(log_likelihood)
+}
+
+fun(thetaA)
+
+ineq=function(theta){
+  
+  z1=theta[5]^2-2*theta[6]*theta[1]
+  z2=theta[4]^2-2*theta[6]*theta[2]
+  # z3=theta[1]
+  # z4=theta[2]
+  return(c(z1,z2))
+}
+
+thetaA=c(.5,1.5,1.5,2)
+y=GND_sim(thetaA,1000)
+
+
+fun(thetaA)
+#thetaa = theta_new = stats::optim(par = thetaA, fun, method = "L-BFGS-B")$par
+theta_new=Rsolnp::solnp(c(.5,1,1,2),fun,
+                        #ineqfun = ineq,ineqUB = c(0,0),ineqLB = c(-Inf,-Inf),
+                        LB=c(-.98,-2,-2,0),UB=c(.98,2,2,5))$par
+
+
+# theta1, theta2, and theta3 constraints
+
+rho=seq(-1,1,by=.1)
+theta1=1/(1-rho^2)
+plot(rho,theta1,type="l",xlab="rho",ylab="theta1",main="theta1 vs rho")
+
+theta3=rho/(1-rho^2)
+plot(rho,theta3,type="l",xlab="rho",ylab="theta3",main="theta3 vs rho")
+
 ###
 source("GND functions.R")
 # run ---------------------------------------------------------------------
 
 library(MASS)
-y=rmvnorm(100,mean=c(0.1,0.05),sigma=matrix(c(1,.6,.6,1),2,2))
+y=mvrnorm(100,mu=c(0.1,0.05),Sigma=matrix(c(1,.6,.6,1),2,2))
 y1=y[,1]
 y2=y[,2]
 
@@ -20,7 +94,9 @@ cdY2=cond_mom(.1,.2,theta2)
 # plot(x,y,type='l',lwd=2)
 
 # Set integral boundaries to -5 and 5 for convergence 
-int=integrate(f=gfun_GND,lower = -5,upper = 5,theta=theta0,sig2AB=cdY$sig2AB)
+int=integrate(f=gfun_GND,lower = -5,upper = 5,theta=theta1
+              #,sig2AB=cdY$sig2AB
+              )
 et=log(int$value)
 eta
 
